274792a0e58fef254f976b40929a21dbdaa9ba3a
Krish Mehta <krish1604mehta@gmail.com>
2025-03-03 12:35:46 +0530

    bg

--- server.js
+++ server.js
@@ -8,6 +8,9 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/api/auth", authRoutes);
-
+app.get("/", (req, res) => {
+  res.send("Krish is Best");
+}
+);
 const PORT = process.env.PORT || 5001;
 app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
